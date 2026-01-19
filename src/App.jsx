cf5bf9eb18272659eda2add92fc12c3f7a56e9db
abdullah97-pro <abdullahahmadi019@gmail.com>
2026-01-19 07:45:01 +0430

    Lazy Loading Product Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { BrowserRouter, Routes } from 'react-router-dom'
-import Home from "./pages/Home";
-import Product from "./pages/Product";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import Home from "./pages/Home";
+// import Product from "./pages/Product";
+import React, {Suspense} from 'react';
+
+const Home = React.lazy(() => import("./pages/Home"));
+const Product = React.lazy(() => import("./pages/Product"));
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element = {<Home />} />
-        <Route path="/product/:id" element = {<Product />} />
-      </Routes>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Routes>
+          <Route path="/" element = {<Home />} />
+          <Route path="/product/:id" element = {<Product />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   )
 }
